63b4979636d04661782b3d52e6c9457893579c4b
ThanhDT <thanh.do@codelovers.vn>
2022-05-20 16:44:26 +0700

    FE web

--- public/js/script.js
+++ public/js/script.js
@@ -1,173 +1,105 @@
-// Menu fixed
-var MenuScroll=function(){
-	$(window).scroll(function(){
-		if($(window).scrollTop() >$('.slider-banner').height()){
-			$('.header-menu').addClass('fixed');
-		}
-		else{
-			$('.header-menu').removeClass('fixed');
 
+// Menu
+var Menu = function(){
+	$('.menu').find("ul li").each(function() {
+		if($(this).find("ul>li").length > 0){
+			$(this).addClass('menu-c2');
 		}
 	});
-};
+}
+
+
+// Menu mobile
+var Menu_mobile=function(){
+	$('.menu-mobile').find("ul li").each(function() {
+		if($(this).find("ul>li").length > 0){
+			$(this).addClass('color');
+			$(this).append('<span>+</span>');
+			$(this).children('span').addClass('menu-c3');
+		}
+	});
+
+	// $('.menu-c3>a').click(function(e){
+	// 	e.preventDefault();
+	// 	$(this).parent().children('ul').stop().slideToggle(500);
+	// });
+	// $('.menu-c3').click(function(e){
+	// 	$(this).children('ul').stop().slideToggle(500);
+	// });
+
+	$('.menu-c3').click(function(){
+		$(this).parent().children('ul').stop().slideToggle(300);
+	});
+
+
+	$('.btn-bars').click(function(e){
+		$('.menu-mobile').toggleClass('open');
+	})
+}
+
+// Email mobile
+var Email_mb=function(){
+	$(window).scroll(function(){
+		if($(window).scrollTop() != 0){
+			$('.email').addClass('hide');
+		}
+		else{
+			$('.email').removeClass('hide');
+		}
+	});
+}
 
 // Back to top
 var Backtotop = function(){
-	$('.btn-top').click(function(){
-		document.body.scrollTop = 0;
-		document.documentElement.scrollTop = 0;
+	$('.back-to-top').click(function(){
+		$("html, body").animate({scrollTop: 0}, 500);
 	});
 };
 
-
-
-// Slider
-$(document).ready(function(){
-	$('.slider-banner').slick({
-		dots: true,
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
-	});
-
-
-
-	// $('.menu').find("ul li").each(function() {
-	// 	if($(this).find("ul>li").length > 0){
-	// 		$(this).append('<i class="fa fa-angle-down btn-drop" aria-hidden="true"></i>');
-	// 	}
-	// });
-	// $('.btn-menu').click(function(){
-	// 	$(this).children('i').toggleClass('fa-bars').toggleClass('fa-close');
- //    	$('.menu').children('ul').slideToggle(200);
-	// });
-
-	// $('.btn-drop').click(function(){
-	// 	$(this).toggleClass('fa-angle-down').toggleClass('fa-angle-up');
-	// 	$(this).parent('li').children('ul').toggleClass('active');
-	// });
-
-
-
-
-	$('.menu1').find("ul li").each(function() {
-		if($(this).find("ul>li").length > 0){
-			$(this).append('<i class="fa fa-angle-down btn-drop1" aria-hidden="true"></i>');
-		}
-	});
-	$('.btn-menu').click(function(){
-    	$('.menu1').addClass('show');
-	});
-	$('.btn-menu1').click(function(){
-		$('.menu1').removeClass('show');
-	});
-	$('.btn-drop1').click(function(){
-		$(this).toggleClass('fa-angle-down').toggleClass('fa-angle-up');
-		$(this).parent('li').children('ul').toggleClass('active1');
-	});
-
-
-
-
-
-
-// input cart
-	var number=$('.input-number').val();
-	$('.plus').click(function(){
-		var val = parseInt(number);
-	  	val= val + 1;
-	  	number= val;
-	  	$('.input-number').val(val);
-	});
-
-	$('.minus').click(function(){
-		var val = parseInt(number);
-	  	if(number ==1){
-	  		val=1;
-	  	}
-	  	else{
-	  		val= val - 1;
-	  	}
-	  	$('.input-number').val(val);
-
-	  	number = val;
-
-	});
-
-	
-
-	$('.slider-for').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		fade: true,
-		asNavFor: '.slider-nav'
+// Slider - Client List
+var Slider_client_list = function(){
+	if($('.slider-client-list').length>0){
+		$('.slider-client-list').slick({
+			slidesToShow: 2,
+			infinite: false,
+			speed: 400,
+			arrows: true,
+			dots: false,
+			prevArrow: '<button class="btn-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></button>',
+			nextArrow: '<button class="btn-next"><i class="fa fa-angle-right" aria-hidden="true"></i></button>',
+			// responsive: [
+			// 	{
+			// 		breakpoint: 1024,
+			// 		settings: {
+			// 			slidesToShow: 3,
+			// 			slidesToScroll: 3,
+			// 			infinite: true,
+			// 			dots: true
+			// 		}
+			// 	},
+			// 	{
+			// 		breakpoint: 600,
+			// 		settings: {
+			// 			slidesToShow: 2,
+			// 			slidesToScroll: 2
+			// 		}
+			// 	},
+			// 	{
+			// 		breakpoint: 480,
+			// 		settings: {
+			// 			slidesToShow: 1,
+			// 			slidesToScroll: 1
+			// 		}
+			// 	}
+			// ]
 		});
-	$('.slider-nav').slick({
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		asNavFor: '.slider-for',
-		dots: true,
-		focusOnSelect: true,
-		arrows: false,
-		dots: false,
-		
-		responsive: [
-		    {
-		      breakpoint: 767,
-		      settings: {
-		        slidesToShow: 4,
-		        slidesToScroll: 4
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        slidesToShow: 3,
-		        slidesToScroll: 3
-		      }
-		    }
-
-		  ]
-		});
-});
+	}
+};
 
 $(function(){
-	MenuScroll();
+	Menu_mobile();
+	Menu();
+	Email_mb();
 	Backtotop();
+	Slider_client_list();
 });
-
-
-
-
-// Footer
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.2';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
-// news-detail
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.2';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.2';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,199 +16,46 @@
     @stack('css')
 
 
-    <!-- Bootstrap Css -->
-    <link href="{{ asset('css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css">
-    <!-- Toastr Css -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('libs\toastr\build\toastr.min.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/font-awesome.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/animate.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/media.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('scss/style.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('scss/media.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('slick/slick.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/select2.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('jquery-fancybox/dist/jquery.fancybox.min.css') }}" type="text/css">
 </head>
 <body>
-    <header>
-        <div class="header">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-2 col-sm-2 col-xs-2 logo">
-                        {{-- <a href="{{ route('web.index') }}" title="">
-                            <img title="" class="img-responsive" src="{{ asset('images/web/logo.jpg') }}" alt="">
-                        </a> --}}
-                    </div>
-                    <div class="col-md-10 col-sm-10 col-xs-10">
-                        <div class="phone">
-                            <i class="fa fa-phone-square" aria-hidden="true"></i> <a href="tel:0563047024" title="">0563.047.024</a>
-                        </div>
-                        <div class="email">
-                            <i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:ducthang.dt03@gmail.com">ducthang.dt03@gmail.com</a>
-                        </div>
-                        @if (auth()->guard('web')->user())
-                            <div class="dropdown d-inline-block-sm float-right account">
-                                <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{ auth()->guard('web')->user()->name }} <i class="fa fa-angle-down" aria-hidden="true"></i>
-                                </button>
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <a class="dropdown-item" href="{{ route('web.profile') }}" title="Thông tin cá nhân">Thông tin cá nhân</a>
-                                    <a class="dropdown-item" href="{{ route('web.change-password') }}" title="Đổi mật khẩu">Đổi mật khẩu</a>
-                                    <form method="POST" action="{{ route('web.logout') }}">
-                                        @csrf
-                                        <a class="dropdown-item text-danger" href="{{ route('web.logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">Đăng xuất</a>
-                                    </form>
-                                </div>
-                            </div>
-                        @else
-                            <div class="login ml-3">
-                                <a href="{{ route('web.login') }}" title="Đăng nhập">Đăng nhập</a>
-                            </div>
-
-                            <div class="register ml-3">
-                                <a href="{{ route('web.register') }}" title="Đăng ký">Đăng ký</a>
-                            </div>
-                        @endif
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="header-menu">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-2 col-sm-2 col-xs-2 logo">
-                    </div>
-                    <div class="col-md-10 col-sm-10 col-xs-10 rip">
-                        <button class="btn-menu"><i class="fa fa-bars" aria-hidden="true"></i></button>
-                        <div class="menu">
-                            <ul>
-                                <li><a href="{{ route('web.index') }}" title="Trang chủ">Trang chủ</a></li>
-                                </li>
-                                <li><a href="javascript:void(0)" title="Danh mục sản phẩm">Danh mục sản phẩm</a>
-                                    <ul>
-                                    @foreach ($category_products as $category)
-                                        <li><a href="{{ route('web.category-product', $category->id) }}" title="{{ $category->name }}">{{ $category->name }}</a></li>
-                                    @endforeach
-                                    </ul>
-                                </li>
-                                <li><a href="javascript:void(0)" title="Tin tức">Tin tức</a>
-                                    <ul>
-                                        @foreach ($category_news as $category)
-                                          <li><a href="{{ route('web.category-news', $category->id) }}" title="{{ $category->name }}">{{ $category->name }}</a></li>
-                                        @endforeach
-                                    </ul>
-                                </li>
-                                <li><a href="{{ route('web.contact') }}" title="Liên hệ">Liên hệ</a></li>
-                                @if (auth()->guard('web')->user())
-                                    <li><a href="{{ route('web.view-post-product') }}" title="Đăng sản phẩm">Đăng sản phẩm</a></li>
-                                @endif
-                            </ul>
-                        </div>
-
-                        <div class="menu1">
-                            <button class="btn-menu1"><i class="fa fa-close" aria-hidden="true"></i></button>
-                            <ul>
-                                <li><a href="{{ route('web.index') }}" title="Trang chủ">Trang chủ</a></li>
-                                </li>
-                                <li><a href="javascript:void(0)" title="Danh mục sản phẩm">Danh mục sản phẩm</a>
-                                    <ul>
-                                      @foreach ($category_products as $category)
-                                        <li><a href="{{ route('web.category-product', $category->id) }}" title="{{ $category->name }}">{{ $category->name }}</a></li>
-                                      @endforeach
-                                    </ul>
-                                </li>
-                                <li><a href="javascript:void(0)" title="Tin tức">Tin tức</a>
-                                    <ul>
-                                        @foreach ($category_news as $category)
-                                          <li><a href="{{ route('web.category-news', $category->id) }}" title="{{ $category->name }}">{{ $category->name }}</a></li>
-                                        @endforeach
-                                    </ul>
-                                </li>
-                                <li><a href="{{ route('web.contact') }}" title="Liên hệ">Liên hệ</a></li>
-                                @if (auth()->guard('web')->user())
-                                    <li><a href="{{ route('web.view-post-product') }}" title="Đăng sản phẩm">Đăng sản phẩm</a></li>
-                                    <li><a href="{{ route('web.profile') }}" title="Thông tin cá nhân">Thông tin cá nhân</a></li>
-                                    <li><a href="{{ route('web.change-password') }}" title="Đổi mật khẩu">Đổi mật khẩu</a></li>
-                                    <li>
-                                        <form method="POST" action="{{ route('web.logout') }}">
-                                            @csrf
-                                            <a class="dropdown-item text-danger" href="{{ route('web.logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">Đăng xuất</a>
-                                        </form>
-                                    </li>
-                                @else
-                                    <li><a href="{{ route('web.login') }}" title="Đăng nhập">Đăng nhập</a></li>
-                                    <li><a href="{{ route('web.register') }}" title="Đăng ký">Đăng ký</a></li>
-                                @endif
-                            </ul>
-                        </div>
-                        <div class="search">
-                            <form action="{{ route('web.search') }}">
-                                <input type="text" placeholder="Từ khóa tìm kiếm..." name="search">
-                                <button type="submit" title=""><i class="fa fa-search" aria-hidden="true"></i></button>
-                            </form>
-                        </div>
+    <header class="header-index">
+        <div class="container">
+            <div class="row">
+                <div class="col-auto">
+                    <a href="#" title="" class="c-img logo">
+                        <img src="{{ asset('images/logo.png') }}" alt="" title="">
+                    </a>
+                    <h1>Asia & Beyond</h1>
+                    <a href="#" title="" class="link-web">Video Production</a>
+                    <div class="icon">
+                        <a href="#" title=""><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+                        <a href="#" title=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href="#" title=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        <a href="#" title=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
                     </div>
                 </div>
             </div>
         </div>
     </header>
-    
-    <!--          Content -->
-    @yield('content')
-    <!--          Content -->
+    <video loop muted autoplay class="video-index">
+        <source src="{{ asset('video/video.mp4') }}" type="video/mp4">
+    </video>
 
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-sm-4 col-xs-12 info-contact p-top30">
-                    <h2>THÔNG TIN LIÊN HỆ</h2>
-                    <p>Địa chỉ: Km 10, Đường Nguyễn Trãi, Quận Thanh Xuân , TP Hà Nội, Việt Nam</p>
-                    <div><span>Hotline:</span> <a href="tel:0563047024">0563.047.024</a></div>
-                    <div><span>Email:</span> <a href="mailto:ducthang.dt03@gmail.com">ducthang.dt03@gmail.com</a></div>
-                    <p>Giấy CNĐKDN: 0308808576</p>
-                </div>
-                <div class="col-md-5 col-sm-8 col-xs-12 p-top30">
-                    <div class="row">
-                        <div class="col-md-6 col-sm-6 col-xs-12 info-footer">
-                            <h3>THÔNG TIN</h3>
-                            <a href="#" title="">Thông tin TK ngân hàng</a>
-                            <a href="#" title="">Thông tin liên hệ</a>
-                            <img title="" class="img-responsive" src="{{ asset('images/web/dky.png') }}" alt="">
-                        </div>
-                        <div class="col-md-6 col-sm-6 col-xs-12 sup-footer">
-                            <h3>HỖ TRỢ KHÁCH HÀNG</h3>
-                            <a href="#" title="">Hình thức thanh toán</a>
-                            <a href="#" title="">Chính sách</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-12 col-xs-12 p-top30">
-                    <div class="fb-page" 
-                        data-href="https://www.facebook.com/facebook"
-                        data-width="380" 
-                        data-hide-cover="false"
-                        data-show-facepile="false">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-
-
-    <a href="tel:0564654654" title="" class="btn-call">
-        <span><i class="fa fa-phone"></i></span>
-    </a>
-
-    <button class="btn-top"><i class="fa fa-angle-up"></i></button>
-
-
-
-    <script src="{{ asset('libs/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-2.2.1.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/script.js') }}"></script>
     <script src="{{ asset('slick/slick.js') }}"></script>
-    <!-- toastr plugin -->
-    <script src="{{ asset('libs\toastr\build\toastr.min.js') }}"></script>
+    <script src="{{ asset('js/select2.min.js') }}"></script>
+    <script src="{{ asset('jquery-fancybox/dist/jquery.fancybox.min.js') }}"></script>
 
     <script type="text/javascript">
         $.ajaxSetup({
